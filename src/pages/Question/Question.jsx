89a8b2af9d862662b88f769a0a8491b101e0e3b4
foxoffire33 <reinierdlp@gmail.com>
2024-04-16 15:29:37 +0200

    some styleing

--- src/pages/Question/Question.jsx
+++ src/pages/Question/Question.jsx
@@ -3,15 +3,27 @@ import './Question.css'
 import { useQuestionsStore } from '../../stores/questions';
 
 function Question() {
-  const { questions, foundRecipes, matchAnswersAndFindRecipes, handleAnswerChange } = useQuestionsStore((state) => state)
+  const { questions, foundRecipes, matchAnswersAndFindRecipes, handleAnswerChange, loadRecipies } = useQuestionsStore((state) => state)
 
+  useEffect(() => {
+    loadRecipies().catch(console.error)
+  }, [])
+
+  // Components
   const Card = ({ recipe }) => {
     return (
-      <div className="card">
-        {recipe.image && <img src={recipe.image} alt={recipe.label} className="card-image" />}
-        <div className="card-content">
-          <h3 className="card-title">{recipe.label}</h3>
-          <p className="card-description">
+      <div className='card'>
+        {recipe.image &&
+          <img
+            src={recipe.image}
+            alt={recipe.label}
+            className="card-image"
+            style={{ backgroundColor: 'orange' }}
+          />
+        }
+        <div className='card-content'>
+          <h3 className='card-title'>{recipe.label}</h3>
+          <p className='card-description'>
             <a href={recipe.url}>View Recipe</a>
           </p>
         </div>
@@ -22,11 +34,11 @@ function Question() {
   return (
     <div style={{ paddingTop: '100px', margin: 'auto', width: '95%', padding: '100px, 10px, 10px, 10px', display: 'grid', gridTemplateColumns: '35% 55%' }}>
       <div>
-        <h2>Recipes Quiz</h2>
+        <h2 style={{ fontSize: '3em', color: 'rgb(255, 165, 0)', marginTop: '1em' }}>Recipes Quiz</h2>
         {questions &&
           <form onSubmit={(e) => {
             e.preventDefault()
-
+            window.scrollTo({ top: 0, behavior: 'smooth' });
           }
 
           }>
@@ -37,28 +49,34 @@ function Question() {
                     {(index + 1)}. {question.question}
                   </b>
                 </p>
-                <ul>
+                <ul style={{ padding: 0, margin: 0, listStyle: 'none' }}>
                   {question?.options.map((opt, index) => (
-                    <li key={index} style={{ listStyle: 'none' }}>
-                      <input type="radio" name={`question${question.id}`} value={opt.value} onChange={() => handleAnswerChange(question.id, index)} required />
+                    <li key={index} style={{ borderBottom: '1px solid #ddd', padding: '5px 10px' }}>
+                      <input type='radio' name={`question${question.id}`} value={opt.value} onChange={() => handleAnswerChange(question.id, index)} required />
                       &nbsp;{opt.label}
                     </li>
                   ))}
                 </ul>
-
               </div>
             ))}
-            <input type="submit" value="submit" onClick={() => matchAnswersAndFindRecipes(useQuestionsStore.getState())} />
+            <input type='submit' value='Zoek recepten' onClick={() => matchAnswersAndFindRecipes(useQuestionsStore.getState())}
+              style={{
+                backgroundColor: 'orange',
+                color: 'white',
+                fontSize: '1.2rem',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                border: 'none',
+                cursor: 'pointer',
+              }}
+            />
           </form>
         }
       </div>
       <div>
-        <h2>Recipes</h2>
+        <h2 style={{ fontSize: '3em', color: 'rgb(255, 165, 0)', marginTop: '1em' }}>Quiz results</h2>
         {foundRecipes.length === 0 ? (<p>Geen recepten gevonden. vul eerst de vragen in.</p>) : (
-          // foundRecipes.map(foundRecipie => (
-          //   <Cards key={foundRecipie.id} rec={foundRecipie} />
-          // ))
-          <div className="card-list">
+          <div className='card-list'>
             {foundRecipes.map((recipe, index) => (
               <Card key={index} recipe={recipe} />
             ))}
--- src/stores/questions.js
+++ src/stores/questions.js
@@ -1,5 +1,7 @@
 import { create } from 'zustand'
 
+const { VITE_EDAMAM_APP_ID, VITE_EDAMAM_APP_KEY } = import.meta.env;
+
 export const useQuestionsStore = create((set) => ({
     questions: [
         {
@@ -56,27 +58,19 @@ export const useQuestionsStore = create((set) => ({
         },
     ],
     answers: [],
-    recipes: [
-        {
-            'id': 1,
-            'uri': 'http://www.edamam.com/ontologies/edamam.owl#recipe_1f0bce8a9dd6a301a4c661b3e66ca8cd',
-            'label': 'Fish Tacos With Sweet Curried Broccoli Sla',
-            'image': 'https://edamam-product-images.s3.amazonaws.com/web-img/063/063054203e49bd012fbaa81046846b9b.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIP%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJIMEYCIQCVH1ZtcPmNV4zfbT5HQV48fQAZyA%2FWAqFKgLWhp8YvogIhAPVQir3lWMFV3UKNql49zrv605HmKdiT0dIguh2kF4J%2FKsEFCLz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMMTg3MDE3MTUwOTg2IgxYU144B2%2FqycNw9uYqlQVNJBAwk2h927ocqOaNReY87mlP7NsdvIFCp%2F2x9XpcKkIc9vUP5faOrnHCes8tI6ch7UBkrAMgGQboHcP4UvsvcAKpOMRdf3eyo2IqWPNWBs%2BNyjpsgSNKaF1eEbm6HTllKnDQ40dB3rICdhMwMtFNoREqbNJiIB8UX9TDMCyrsuRKZpBSy%2FDqwVwBludWa8evzjQB5aaTh6E4tbPs5zwToOoSqnRTDTgZC7AhZ8i48q7CeyDr48rTk9%2BP5bgNV1TYyrSUATd3tgx5Iu8l50rRmiETvecPmF6e5upQNaurG3V5VdPewJ%2FmuWmXWdHtYkwnRDLvmvAz4jyeMHZUwyL6asUVjXocy5xkwT9Tn7nCPrNG%2BP3PWL3LYdAJcivYcHmhEPjkOteI6tmeCoGRRyjvg5sTRjtuKgQ9DR8idD%2BSgwMbdBy5dlzGZc8EUQLFVB30roC5PlPkDu%2FS9WLZkvtMF%2BfJ%2BEjkxxkF92qrjxejuxgUCi3ge%2BF9aHMcXLhpIyMleW3nHYmhM9aId7EDnvn%2BZEjbEPKBdEo%2BXMbYe6M8wsFgRpIGEsxiRPlniCBz5t8TuAFJV48xTl4dr%2Bs723A%2BSBGDWMjhFpkq5HqN87Maz7MCHLPnLSD1emi3mVVpbkdlvEABhCWRqesV0v6%2Fi8WAz%2BKL8KiUOqaeKoCNrr0H9FrgrbMr4koAnlDLRyw29vnavfw1QJwgWaLquDjeHxIGCETOksyMswAUuAED1PdAUlONT6FFbPcqk0vmdkwCYXJPyc4SDdRfM5u5IGxEP7T3vBMVU8qtDseqRDGvlPHT0qyYHQRqvF6hprwADQg5pksPz1CKbi0RoSql3zGcX7f1mUiY9a2Quhp60UyK%2ForDk7jl2qFmMOqt%2BbAGOrABmkC40dG5w0pPrLzrA5uATDtp4EFzv8s0LwGgKlusAFussPoqREWaksO1mMUAXRzvVwOAOrk2kY9%2BVLwGQltbkm2%2BCDwydjjgvbRmTxPaGYQorBqabTUtlbVJeCpQUmYrlNLrQ5od2EW7p5AXcxQlSVh1MnVdA1y7V1hznB%2BjzgR9VODYu2goJhf6vObu96KeVvsn%2FkmFS%2BUy0HlElQjULsakV67xPiCLeXMnoIDj0rI%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240416T112531Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFORTYXGA5%2F20240416%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=ba0d4de3aab35cfde9a1b496797659d8208d997388e8137998e94c4395617051',
-            'source': 'zagleft.com',
-            'url': 'http://zagleft.com/recipe/fish-tacos-with-curried-broccoli-slaw-for-one/',
-            'shareAs': 'http://www.edamam.com/recipe/fish-tacos-with-sweet-curried-broccoli-sla-1f0bce8a9dd6a301a4c661b3e66ca8cd/sla',
-        },
-        {
-            'id': 2,
-            'uri': 'http://www.edamam.com/ontologies/edamam.owl#recipe_8092164669fd9111dd78351738fc6a91',
-            'label': 'Carolina Style BBQ Pork with Pineapple Sla',
-            'image': 'https://edamam-product-images.s3.amazonaws.com/web-img/063/063054203e49bd012fbaa81046846b9b.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIP%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJIMEYCIQCVH1ZtcPmNV4zfbT5HQV48fQAZyA%2FWAqFKgLWhp8YvogIhAPVQir3lWMFV3UKNql49zrv605HmKdiT0dIguh2kF4J%2FKsEFCLz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMMTg3MDE3MTUwOTg2IgxYU144B2%2FqycNw9uYqlQVNJBAwk2h927ocqOaNReY87mlP7NsdvIFCp%2F2x9XpcKkIc9vUP5faOrnHCes8tI6ch7UBkrAMgGQboHcP4UvsvcAKpOMRdf3eyo2IqWPNWBs%2BNyjpsgSNKaF1eEbm6HTllKnDQ40dB3rICdhMwMtFNoREqbNJiIB8UX9TDMCyrsuRKZpBSy%2FDqwVwBludWa8evzjQB5aaTh6E4tbPs5zwToOoSqnRTDTgZC7AhZ8i48q7CeyDr48rTk9%2BP5bgNV1TYyrSUATd3tgx5Iu8l50rRmiETvecPmF6e5upQNaurG3V5VdPewJ%2FmuWmXWdHtYkwnRDLvmvAz4jyeMHZUwyL6asUVjXocy5xkwT9Tn7nCPrNG%2BP3PWL3LYdAJcivYcHmhEPjkOteI6tmeCoGRRyjvg5sTRjtuKgQ9DR8idD%2BSgwMbdBy5dlzGZc8EUQLFVB30roC5PlPkDu%2FS9WLZkvtMF%2BfJ%2BEjkxxkF92qrjxejuxgUCi3ge%2BF9aHMcXLhpIyMleW3nHYmhM9aId7EDnvn%2BZEjbEPKBdEo%2BXMbYe6M8wsFgRpIGEsxiRPlniCBz5t8TuAFJV48xTl4dr%2Bs723A%2BSBGDWMjhFpkq5HqN87Maz7MCHLPnLSD1emi3mVVpbkdlvEABhCWRqesV0v6%2Fi8WAz%2BKL8KiUOqaeKoCNrr0H9FrgrbMr4koAnlDLRyw29vnavfw1QJwgWaLquDjeHxIGCETOksyMswAUuAED1PdAUlONT6FFbPcqk0vmdkwCYXJPyc4SDdRfM5u5IGxEP7T3vBMVU8qtDseqRDGvlPHT0qyYHQRqvF6hprwADQg5pksPz1CKbi0RoSql3zGcX7f1mUiY9a2Quhp60UyK%2ForDk7jl2qFmMOqt%2BbAGOrABmkC40dG5w0pPrLzrA5uATDtp4EFzv8s0LwGgKlusAFussPoqREWaksO1mMUAXRzvVwOAOrk2kY9%2BVLwGQltbkm2%2BCDwydjjgvbRmTxPaGYQorBqabTUtlbVJeCpQUmYrlNLrQ5od2EW7p5AXcxQlSVh1MnVdA1y7V1hznB%2BjzgR9VODYu2goJhf6vObu96KeVvsn%2FkmFS%2BUy0HlElQjULsakV67xPiCLeXMnoIDj0rI%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240416T112531Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFORTYXGA5%2F20240416%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=ba0d4de3aab35cfde9a1b496797659d8208d997388e8137998e94c4395617051',
-            'source': 'jennsfarmtable.com',
-            'url': 'http://jennsfarmtable.com/2016/06/carolina-style-bbq-pork-pineapple-slaw/',
-            'shareAs': 'http://www.edamam.com/recipe/carolina-style-bbq-pork-with-pineapple-sla-8092164669fd9111dd78351738fc6a91/sla',
-        }
-    ],
+    recipes: [],
     foundRecipes: [],
+    loadRecipies: async () => {
+        try {
+            const response = await fetch(`https://api.edamam.com/search?q=${'any'}&app_id=${VITE_EDAMAM_APP_ID}&app_key=${VITE_EDAMAM_APP_KEY}&to=100`);
+            const hits = (await response.json()).hits;
+            const recipes = hits.map(h => h.recipe);
+            set((state) => ({ ...state, recipes: recipes }));
+        } catch {
+            set((state) => ({ ...state, recipes: [] }));
+        }
+
+    },
     handleAnswerChange: (questionId, index) => {
         set((state) => ({ answers: { ...state.answers, [questionId]: index } }));
     },
@@ -91,7 +85,7 @@ export const useQuestionsStore = create((set) => ({
     matchAnswersAndFindRecipes: (state) => {
         const answersMatrix = [{
             answers: [1, 1, 1, 1, 1],
-            recipes: [0, 1]
+            recipes: [0, 1, 2, 4, 8, 16, 32, 64]
         }, {}];
 
         try {
@@ -107,7 +101,7 @@ export const useQuestionsStore = create((set) => ({
             if (result !== undefined) {
                 const recipes = result?.recipes?.map(rid => state.recipes[rid])
                 set(() => ({ foundRecipes: recipes }))
-            }else {
+            } else {
                 set(() => ({ foundRecipes: [] }))
             }
         } catch (error) {
